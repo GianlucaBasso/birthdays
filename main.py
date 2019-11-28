6b86a9014090ebc5a83059315198988a6524924e
dostodejski <47861797+dostodejski@users.noreply.github.com>
2019-11-28 17:22:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 from birthdays import return_birthday
-from birthdays import my_package
+from my_package import birthdays.py
 
 return_birthday('Albert Einstein was a Genirus')
 return_birthday('Andrea Marino was cooking carbonara')
